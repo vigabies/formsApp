fac7847bd8f0e2377c30907b19fb10e68ca9c54f
Gabriela Silva <vigabiesdivision@gmail.com>
2022-11-24 10:54:31 -0300

    registro.page.ts

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formRegistro: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formRegistro = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+     //a gente n vai validar o cpf, mas vamos deixar ele obrigatório
+      cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(11)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(8)])],
+      confirmaSenha: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(8)])]
+    });
+   }
 
   ngOnInit() {
   }
+
 
 }
